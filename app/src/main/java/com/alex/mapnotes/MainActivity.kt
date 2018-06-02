919d5d920ee50dd82b26dd8d1efeb4d49c76614e
Alex Zhukovich <zhukovich.a@gmail.com>
2018-06-02 13:18:09 +0200

    Added BottomSheet to MainActivity; Added SaveNoteFragment and SearchNotesFragment;

--- app/src/main/java/com/alex/mapnotes/MainActivity.kt
+++ app/src/main/java/com/alex/mapnotes/MainActivity.kt
@@ -2,33 +2,49 @@ package com.alex.mapnotes
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.BottomSheetBehavior
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.navigation
+import kotlinx.android.synthetic.main.button_sheet.bottomSheet
 
 class MainActivity : AppCompatActivity() {
+
+    private val bottomSheetBehavior by lazy {
+        BottomSheetBehavior.from(bottomSheet)
+    }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_add_note -> {
-                message.setText(R.string.nav_add_note_title)
+                replaceBottomFragment(SaveNoteFragment())
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_map -> {
-                message.setText(R.string.nav_map_title)
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_search_notes -> {
-                message.setText(R.string.nav_search_notes_title)
+                replaceBottomFragment(SearchNotesFragment())
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
 
+    private fun replaceBottomFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.bottomSheetContainer, fragment)
+                .commit()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
