f09cfe187e6b3896df1721c7281bb54c7c665973
Alex Zhukovich <zhukovich.a@gmail.com>
2018-10-09 00:18:58 +0200

    Added ViewTextIdlingResource and ViewVisibilityIdlingResource classes; Added E2E test cases for SignInActivity

---
+++ app/src/androidTest/java/com/alex/mapnotes/matchers/ViewVisibilityIdlingResource.kt
@@ -0,0 +1,47 @@
+package com.alex.mapnotes.matchers
+
+import android.app.Activity
+import android.view.View
+import androidx.annotation.IdRes
+import androidx.test.espresso.IdlingResource
+import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry
+import androidx.test.runner.lifecycle.Stage
+
+class ViewVisibilityIdlingResource(
+    @IdRes private val expectedViewId: Int,
+    private val expectedViewVisibility: Int
+) : IdlingResource {
+
+    private var resourceCallback: IdlingResource.ResourceCallback? = null
+
+    override fun getName(): String {
+        return ViewVisibilityIdlingResource::class.java.simpleName
+    }
+
+    override fun isIdleNow(): Boolean {
+        val view: View? = getActivityInstance().findViewById(expectedViewId)
+        val isIdle = if (view != null) {
+            view.visibility == expectedViewVisibility
+        } else {
+            false
+        }
+
+        if (isIdle && resourceCallback != null) {
+            resourceCallback?.onTransitionToIdle()
+        }
+        return isIdle
+    }
+
+    override fun registerIdleTransitionCallback(callback: IdlingResource.ResourceCallback?) {
+        resourceCallback = callback
+    }
+
+    private fun getActivityInstance(): Activity {
+        var currentActivity: Activity? = null
+        val resumedActivities = ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(Stage.RESUMED)
+        if (resumedActivities.iterator().hasNext()) {
+            currentActivity = resumedActivities.iterator().next()
+        }
+        return currentActivity!!
+    }
+}
