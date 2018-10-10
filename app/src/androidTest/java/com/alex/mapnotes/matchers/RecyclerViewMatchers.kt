004b5e765ce1cee983330dd9c285b6a786bdd045
Alex Zhukovich <zhukovich.a@gmail.com>
2018-10-10 23:33:11 +0200

    Added NonEmptyTextIdlingResource, RecyclerViewSizeIdlingResources classes; Added RecyclerViewMatchers#withItemText matcher; Added BaseTestRobot#matchRecyclerViewItemWithText function; Added additional functions for testing HomeScreen; Added an E2E test case for verification adding and searching data

--- app/src/androidTest/java/com/alex/mapnotes/matchers/RecyclerViewMatchers.kt
+++ app/src/androidTest/java/com/alex/mapnotes/matchers/RecyclerViewMatchers.kt
@@ -2,6 +2,9 @@ package com.alex.mapnotes.matchers
 
 import androidx.recyclerview.widget.RecyclerView
 import android.view.View
+import android.widget.TextView
+import com.alex.mapnotes.R
+import com.alex.mapnotes.search.adapter.NoteViewHolder
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.TypeSafeMatcher
@@ -17,6 +20,28 @@ object RecyclerViewMatchers {
 
             override fun describeTo(description: Description) {
                 description.appendText("RecyclerView should have $count items")
+            }
+        }
+    }
+
+    fun withItemText(text: String): Matcher<View> {
+        return object : TypeSafeMatcher<View>() {
+            override fun matchesSafely(view: View): Boolean {
+                val recyclerView = view as RecyclerView
+                val adapter = recyclerView.adapter
+                val itemCount = adapter?.itemCount!!
+                for (pos in 0..itemCount) {
+                    val viewHolder = recyclerView.findViewHolderForAdapterPosition(pos) as NoteViewHolder
+                    val noteTextView = viewHolder.itemView.findViewById<TextView>(R.id.noteText)
+                    if (noteTextView.text.toString() == text) {
+                        return true
+                    }
+                }
+                return false
+            }
+
+            override fun describeTo(description: Description) {
+                description.appendText("RecyclerView should have item with text: $text")
             }
         }
     }
