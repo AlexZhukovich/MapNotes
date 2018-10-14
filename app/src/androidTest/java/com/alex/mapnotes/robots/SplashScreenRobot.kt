8a9f137a72c91cc9cc79fa1a1481b15e0ac970e8
Alex Zhukovich <zhukovich.a@gmail.com>
2018-10-14 12:12:30 +0200

    Refactoring FirebaseUserRepository class; Added SplashScreenRobot and updated HomeScreenRobot and BaseScreenRobot classes; Refactored SmokeTests class

---
+++ app/src/androidTest/java/com/alex/mapnotes/robots/SplashScreenRobot.kt
@@ -0,0 +1,21 @@
+package com.alex.mapnotes.robots
+
+import androidx.test.rule.ActivityTestRule
+import com.alex.mapnotes.di.appModule
+import com.alex.mapnotes.splash.SplashActivity
+import org.koin.standalone.StandAloneContext
+
+fun splashScreen(func: SplashScreenRobot.() -> Unit) = SplashScreenRobot().apply { func() }
+
+val splashActivityE2ETestRule = object : ActivityTestRule<SplashActivity>(SplashActivity::class.java, true, false) {
+    override fun beforeActivityLaunched() {
+        StandAloneContext.loadKoinModules(listOf(appModule))
+        super.beforeActivityLaunched()
+    }
+}
+
+class SplashScreenRobot : BaseTestRobot() {
+    fun display() {
+        splashActivityE2ETestRule.launchActivity(null)
+    }
+}
