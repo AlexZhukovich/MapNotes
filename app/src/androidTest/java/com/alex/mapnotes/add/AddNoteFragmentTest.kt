33897d5fd36ab9d10d973d73c687c34f071170d3
Alex Zhukovich <zhukovich.a@gmail.com>
2018-10-16 23:45:22 +0200

    Refactored AddNoteFragmentTest class with internal DSL; Added methods for interacting with AddNoteFragment into internal DSL

--- app/src/androidTest/java/com/alex/mapnotes/add/AddNoteFragmentTest.kt
+++ app/src/androidTest/java/com/alex/mapnotes/add/AddNoteFragmentTest.kt
@@ -1,19 +1,11 @@
 package com.alex.mapnotes.add
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.replaceText
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.espresso.matcher.ViewMatchers.withHint
-import androidx.test.espresso.matcher.ViewMatchers.isEnabled
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
 import com.alex.mapnotes.FragmentTestActivity
 import com.alex.mapnotes.R
 import com.alex.mapnotes.di.appModule
-import org.hamcrest.CoreMatchers.not
+import com.alex.mapnotes.robots.homeScreen
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -39,32 +31,27 @@ class AddNoteFragmentTest {
 
     @Test
     fun shouldDisplayNoteHintForANewNote() {
-        onView(withId(R.id.note))
-                .check(matches(withHint(R.string.add_note_hint)))
+        homeScreen {
+            verifyNoteHint(R.string.add_note_hint)
+        }
     }
 
     @Test
     fun shouldChangeAddButtonEnableAfterChangingNoteText() {
-        onView(withId(R.id.add))
-                .check(matches(not(isEnabled())))
-
-        onView(withId(R.id.note))
-                .perform(replaceText(testNoteText))
-
-        onView(withId(R.id.add))
-                .check(matches(isEnabled()))
+        homeScreen {
+            verifyAddButtonIsDisabled()
+            enterNoteText(testNoteText)
+            verifyAddButtonIsEnabled()
+        }
     }
 
     @Test
     fun shouldNothingChangeAfterClickOnAddButtonWithEmptyNoteText() {
-        onView(withId(R.id.note))
-                .check(matches(withText(emptyNoteText)))
-
-        onView(withId(R.id.add))
-                .perform(click())
-
-        onView(withId(R.id.note))
-                .check(matches(withText(emptyNoteText)))
+        homeScreen {
+            verifyNoteText(emptyNoteText)
+            pressAddButton()
+            verifyNoteText(emptyNoteText)
+        }
     }
 
     @After
