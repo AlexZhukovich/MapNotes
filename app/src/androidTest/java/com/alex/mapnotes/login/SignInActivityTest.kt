c69976242ac58103302dc96a524ab04edaaf3a38
Alex Zhukovich <zhukovich.a@gmail.com>
2018-10-17 20:33:53 +0200

    Refactored SignInActivityTest class with internal DSL; Added methods for interacting with SignInActivity into internal DSL

--- app/src/androidTest/java/com/alex/mapnotes/login/SignInActivityTest.kt
+++ app/src/androidTest/java/com/alex/mapnotes/login/SignInActivityTest.kt
@@ -2,32 +2,22 @@ package com.alex.mapnotes.login
 
 import android.content.Intent
 import androidx.test.InstrumentationRegistry
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.replaceText
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.rule.ActivityTestRule
 import androidx.test.rule.GrantPermissionRule
 import androidx.test.runner.AndroidJUnit4
-import com.alex.mapnotes.R
-import com.alex.mapnotes.data.Result
 import com.alex.mapnotes.data.provider.LocationProvider
 import com.alex.mapnotes.data.repository.UserRepository
-import com.alex.mapnotes.home.HomeActivity
 import com.alex.mapnotes.login.signin.SignInActivity
-import com.alex.mapnotes.model.AuthUser
+import com.alex.mapnotes.robots.homeScreen
+import com.alex.mapnotes.robots.prepare
+import com.alex.mapnotes.robots.signInScreen
 import com.alex.mapnotes.testAppModule
-import io.mockk.coEvery
-import io.mockk.every
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.RuleChain
 import org.junit.runner.RunWith
 import org.koin.test.KoinTest
 import org.koin.standalone.StandAloneContext.closeKoin
@@ -36,102 +26,84 @@ import org.koin.standalone.inject
 
 @RunWith(AndroidJUnit4::class)
 class SignInActivityTest : KoinTest {
-    private val incorrectEmail = "test"
+    private val emptyEmail = ""
     private val correctEmail = "test@test.com"
+    private val incorrectEmail = "test"
     private val password = "password"
+    private val emptyPassword = ""
 
     private val userRepository: UserRepository by inject()
     private val locationProvider: LocationProvider by inject()
 
-    @Rule @JvmField
-    val permissionRule: GrantPermissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION)
+    private val permissionRule: GrantPermissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION)
 
-    @Rule @JvmField
-    val activityRule = ActivityTestRule<SignInActivity>(SignInActivity::class.java, true, false)
+    private val activityRule = ActivityTestRule<SignInActivity>(SignInActivity::class.java, true, false)
+
+    @Rule
+    @JvmField
+    val chain: RuleChain = RuleChain
+            .outerRule(permissionRule)
+            .around(activityRule)
 
     @Before
     fun setUp() {
         loadKoinModules(listOf(testAppModule))
         activityRule.launchActivity(Intent(InstrumentationRegistry.getInstrumentation().targetContext, SignInActivity::class.java))
+        Intents.init()
     }
 
     @Test
     fun shouldDisplayEmailErrorWhenEmailIsEmpty() {
-        onView(withId(R.id.signIn))
-                .perform(click())
-
-        onView(withText(R.string.error_email_should_be_valid))
-                .check(matches(isDisplayed()))
+        signInScreen {
+            signIn(emptyEmail, emptyPassword)
+            verifyIncorrectEmailErrorMessage()
+        }
     }
 
     @Test
     fun shouldDisplayEmailErrorWhenEmailIsNotCorrect() {
-        onView(withId(R.id.email))
-                .perform(replaceText(incorrectEmail))
-
-        onView(withId(R.id.signIn))
-                .perform(click())
-
-        onView(withText(R.string.error_email_should_be_valid))
-                .check(matches(isDisplayed()))
+        signInScreen {
+            signIn(incorrectEmail, password)
+            verifyIncorrectEmailErrorMessage()
+        }
     }
 
     @Test
     fun shouldDisplayPasswordErrorWhenPasswordIsEmpty() {
-        onView(withId(R.id.email))
-                .perform(replaceText(correctEmail))
-
-        onView(withId(R.id.signIn))
-                .perform(click())
-
-        onView(withText(R.string.error_password_should_not_be_empty))
-                .check(matches(isDisplayed()))
+        signInScreen {
+            signIn(correctEmail, emptyPassword)
+            verifyEmptyPasswordErrorMessage()
+        }
     }
 
     @Test
     fun shouldDisplaySignInErrorAfterSignInError() {
-        coEvery { userRepository.signIn(any(), any()) } returns Result.Error(Exception("SignIn error"))
-
-        onView(withId(R.id.email))
-                .perform(replaceText(correctEmail))
-
-        onView(withId(R.id.password))
-                .perform(replaceText(password))
-
-        onView(withId(R.id.signIn))
-                .perform(click())
-
-        onView(withText(R.string.error_user_cannot_be_authenticated))
-                .check(matches(isDisplayed()))
+        prepare {
+            mockUnsuccessfulSignInWithException(userRepository)
+        }
+        signInScreen {
+            signIn(correctEmail, password)
+            verifySignInErrorMessage()
+        }
     }
 
     @Test
     fun shouldOpenMapScreenAfterSuccessfulSignIn() {
-        Intents.init()
-        val authUser = AuthUser("111111")
-        every { locationProvider.startLocationUpdates() } answers { nothing }
-        every { locationProvider.stopLocationUpdates() } answers { nothing }
-        every { locationProvider.addUpdatableLocationListener(any()) } answers { nothing }
-        every { locationProvider.isLocationAvailable() } returns false
-        coEvery { userRepository.signIn(correctEmail, password) } returns Result.Success(authUser)
-        coEvery { userRepository.getCurrentUser() } returns Result.Success(authUser)
-
-        onView(withId(R.id.email))
-                .perform(replaceText(correctEmail))
-
-        onView(withId(R.id.password))
-                .perform(replaceText(password))
-
-        onView(withId(R.id.signIn))
-                .perform(click())
-
-        Intents.intended(hasComponent(HomeActivity::class.java.name))
-
-        Intents.release()
+        prepare {
+            mockLocationProvider(locationProvider)
+            mockSuccessfulSignIn(userRepository, correctEmail, password)
+        }
+        signInScreen {
+            signIn(correctEmail, password)
+        }
+        homeScreen {
+            isSuccessfullyLoaded()
+        }
     }
 
     @After
     fun tearDown() {
+        Intents.release()
         closeKoin()
     }
 }
