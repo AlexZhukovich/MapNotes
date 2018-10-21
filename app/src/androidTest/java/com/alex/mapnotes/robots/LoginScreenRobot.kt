ff350df57e1c1ddaa1cde1816068c21cc5c759d9
Alex Zhukovich <zhukovich.a@gmail.com>
2018-10-21 20:54:05 +0200

    Added LoginScreenRobot#isSuccessfullyLoaded, PreparationRobot#mockNoAuthorizedUser, PreparationRobot#mockAuthorizedUser and SplashScreenRobot#displayMock methods; Refactored SplashActivityTest class

--- app/src/androidTest/java/com/alex/mapnotes/robots/LoginScreenRobot.kt
+++ app/src/androidTest/java/com/alex/mapnotes/robots/LoginScreenRobot.kt
@@ -1,6 +1,9 @@
 package com.alex.mapnotes.robots
 
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
 import com.alex.mapnotes.R
+import com.alex.mapnotes.login.LoginActivity
 
 fun loginScreen(func: LoginScreenRobot.() -> Unit) = LoginScreenRobot().apply { func() }
 
@@ -9,4 +12,8 @@ class LoginScreenRobot : BaseTestRobot() {
     fun openSignIn() = clickView(R.id.signIn)
 
     fun openSignUp() = clickView(R.id.signUp)
+
+    fun isSuccessfullyLoaded() {
+        Intents.intended(IntentMatchers.hasComponent(LoginActivity::class.java.name))
+    }
 }
