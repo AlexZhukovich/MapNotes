8454f9a58dcf4eb74dd0eb2de780ed1a5891c7f1
Alex Zhukovich <zhukovich.a@gmail.com>
2018-10-24 22:17:21 +0200

    Rewrote SignUpActivityTest class from Espresso to internal DSL; Added PreparationRobot#mockSignUpError and PreparationRobot#mockSuccessfulSignUp methods; Added SignUpScreenRobot#display, SignUpScreenRobot#signUp, SignUpScreenRobot#isEmailShouldBeValidErrorDisplayed, SignUpScreenRobot#isPasswordShouldNotBeEmptyErrorDisplayed, SignUpScreenRobot#isNameShouldNotBeEmptyErrorDisplayed and SignUpScreenRobot#isAccountCannotBeCreatedErrorDisplayed methods

--- app/src/androidTest/java/com/alex/mapnotes/login/SignUpActivityTest.kt
+++ app/src/androidTest/java/com/alex/mapnotes/login/SignUpActivityTest.kt
@@ -1,159 +1,110 @@
 package com.alex.mapnotes.login
 
-import android.content.Intent
-import androidx.test.InstrumentationRegistry
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.replaceText
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.rule.ActivityTestRule
 import androidx.test.rule.GrantPermissionRule
 import androidx.test.runner.AndroidJUnit4
-import com.alex.mapnotes.R
-import com.alex.mapnotes.data.Result
-import com.alex.mapnotes.data.provider.LocationProvider
-import com.alex.mapnotes.data.repository.UserRepository
-import com.alex.mapnotes.home.HomeActivity
-import com.alex.mapnotes.login.signup.SignUpActivity
-import com.alex.mapnotes.model.AuthUser
-import com.alex.mapnotes.testAppModule
-import io.mockk.coEvery
-import io.mockk.every
+import com.alex.mapnotes.MockTest
+import com.alex.mapnotes.robots.homeScreen
+import com.alex.mapnotes.robots.prepare
+import com.alex.mapnotes.robots.signUpActivity
+import com.alex.mapnotes.robots.signUpScreen
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.koin.standalone.StandAloneContext.closeKoin
-import org.koin.standalone.StandAloneContext.loadKoinModules
-import org.koin.standalone.inject
-import org.koin.test.KoinTest
 
 @RunWith(AndroidJUnit4::class)
-class SignUpActivityTest : KoinTest {
+class SignUpActivityTest : MockTest() {
     private val username = "testUserName"
+    private val emptyUsername = ""
     private val incorrectEmail = "test"
     private val correctEmail = "test@test.com"
+    private val emptyEmail = ""
     private val password = "password"
+    private val emptyPassword = ""
 
-    private val locationProvider: LocationProvider by inject()
-    private val userRepository: UserRepository by inject()
     @Rule @JvmField
     val permissionRule: GrantPermissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION)
 
     @Rule @JvmField
-    val activityRule = ActivityTestRule<SignUpActivity>(SignUpActivity::class.java, true, false)
+    val activityRule = signUpActivity
 
     @Before
-    fun setUp() {
-        loadKoinModules(listOf(testAppModule))
-        activityRule.launchActivity(Intent(InstrumentationRegistry.getInstrumentation().targetContext, SignUpActivity::class.java))
+    override fun setUp() {
+        super.setUp()
     }
 
     @Test
     fun shouldDisplayEmailErrorWhenEmailIsEmpty() {
-        onView(withId(R.id.signUp))
-                .perform(click())
-
-        onView(withText(R.string.error_email_should_be_valid))
-                .check(matches(isDisplayed()))
+        signUpScreen {
+            display()
+            signUp(emptyUsername, emptyEmail, emptyPassword)
+            isEmailShouldBeValidErrorDisplayed()
+        }
     }
 
     @Test
     fun shouldDisplayEmailErrorWhenEmailIsNotCorrect() {
-        onView(withId(R.id.email))
-                .perform(replaceText(incorrectEmail))
-
-        onView(withId(R.id.signUp))
-                .perform(click())
-
-        onView(withText(R.string.error_email_should_be_valid))
-                .check(matches(isDisplayed()))
+        signUpScreen {
+            display()
+            signUp(emptyUsername, incorrectEmail, emptyPassword)
+            isEmailShouldBeValidErrorDisplayed()
+        }
     }
 
     @Test
     fun shouldDisplayPasswordErrorWhenPasswordIsEmpty() {
-        onView(withId(R.id.email))
-                .perform(replaceText(correctEmail))
-
-        onView(withId(R.id.signUp))
-                .perform(click())
-
-        onView(withText(R.string.error_password_should_not_be_empty))
-                .check(matches(isDisplayed()))
+        signUpScreen {
+            display()
+            signUp(emptyUsername, correctEmail, emptyPassword)
+            isPasswordShouldNotBeEmptyErrorDisplayed()
+        }
     }
 
     @Test
     fun shouldDisplayNameErrorWhenNameIsEmpty() {
-        onView(withId(R.id.email))
-                .perform(replaceText(correctEmail))
-
-        onView(withId(R.id.password))
-                .perform(replaceText(password))
-
-        onView(withId(R.id.signUp))
-                .perform(click())
-
-        onView(withText(R.string.error_name_should_not_be_empty))
-                .check(matches(isDisplayed()))
+        signUpScreen {
+            display()
+            signUp(emptyUsername, correctEmail, password)
+            isNameShouldNotBeEmptyErrorDisplayed()
+        }
     }
 
     @Test
     fun shouldDisplaySignUpErrorAfterSignUpError() {
-        coEvery { userRepository.signUp(any(), any()) } returns Result.Error(Exception("SignUp error"))
-
-        onView(withId(R.id.name))
-                .perform(replaceText(username))
-
-        onView(withId(R.id.email))
-                .perform(replaceText(correctEmail))
-
-        onView(withId(R.id.password))
-                .perform(replaceText(password))
-
-        onView(withId(R.id.signUp))
-                .perform(click())
-
-        onView(withText(R.string.error_account_cannot_be_created))
-                .check(matches(isDisplayed()))
+        prepare(testScope) {
+            mockSignUpError()
+        }
+        signUpScreen {
+            display()
+            signUp(username, correctEmail, password)
+            isAccountCannotBeCreatedErrorDisplayed()
+        }
     }
 
     @Test
     fun shouldOpenMapScreenAfterSuccessfulSignUp() {
         Intents.init()
-        val authUser = AuthUser("111111")
-        every { locationProvider.startLocationUpdates() } answers { nothing }
-        every { locationProvider.stopLocationUpdates() } answers { nothing }
-        every { locationProvider.addUpdatableLocationListener(any()) } answers { nothing }
-        every { locationProvider.isLocationAvailable() } returns false
-        coEvery { userRepository.changeUserName(authUser, username) } answers { nothing }
-        coEvery { userRepository.signUp(correctEmail, password) } returns Result.Success(authUser)
-        coEvery { userRepository.getCurrentUser() } returns Result.Success(authUser)
 
-        onView(withId(R.id.name))
-                .perform(replaceText(username))
-
-        onView(withId(R.id.email))
-                .perform(replaceText(correctEmail))
-
-        onView(withId(R.id.password))
-                .perform(replaceText(password))
-
-        onView(withId(R.id.signUp))
-                .perform(click())
-
-        Intents.intended(hasComponent(HomeActivity::class.java.name))
+        prepare(testScope) {
+            mockLocationProvider(isLocationAvailable = false)
+            mockSuccessfulSignUp(username, correctEmail, password)
+            mockAuthorizedUser()
+        }
+        signUpScreen {
+            display()
+            signUp(username, correctEmail, password)
+        }
+        homeScreen {
+            isSuccessfullyLoaded()
+        }
 
         Intents.release()
     }
 
     @After
-    fun tearDown() {
-        closeKoin()
+    override fun tearDown() {
+        super.tearDown()
     }
 }
