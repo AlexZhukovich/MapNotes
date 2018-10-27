e32318cac4a6045eb706230fb3d2b896de9a692c
Alex Zhukovich <zhukovich.a@gmail.com>
2018-10-27 16:14:54 +0200

    Refactored BaseTestRobot

--- app/src/androidTest/java/com/alex/mapnotes/robots/NoLocationPermissionScreenRobot.kt
+++ app/src/androidTest/java/com/alex/mapnotes/robots/NoLocationPermissionScreenRobot.kt
@@ -8,9 +8,9 @@ fun noLocationPermissionScreen(func: NoLocationPermissionScreenRobot.() -> Unit)
 class NoLocationPermissionScreenRobot : BaseTestRobot() {
 
     fun isSuccessfullyLoaded() {
-        matchDisplayedView(R.id.mapImage)
-        matchText(R.id.permissionExplanation, R.string.permission_explanation)
-        matchText(R.id.openAppPrefs, R.string.open_app_prefs)
-        matchViewIsEnabled(R.id.openAppPrefs)
+        isViewDisplayed(R.id.mapImage)
+        isViewWithTextDisplayed(R.id.permissionExplanation, R.string.permission_explanation)
+        isViewWithTextDisplayed(R.id.openAppPrefs, R.string.open_app_prefs)
+        isViewEnabled(R.id.openAppPrefs)
     }
 }
